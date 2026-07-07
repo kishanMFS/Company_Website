1beaf57d1740c66d861171b52c88445aaeaf564d
kishanpp <kish101993@gmail.com>
2026-07-07 16:29:23 +0530

    refact: update dir structure

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost']
+  }
+};
+
+export default nextConfig;
